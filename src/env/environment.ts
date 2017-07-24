aa5b8cebdc89af2cf74da1362e30bf842891dac7
Todd <todd@grooveio.com>
2017-07-24 08:40:52 -0700

    fix

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,14 +1,14 @@
 import { InjectionToken } from '@angular/core';
 
-// Development Env. variables
-export const environment = 'development';
+// Production Env. variables
+export const environment = 'production';
 export const env = environment;
 export const ENV = env;
 
 export const API_PATH = '/api';
-export const API_BASE_HOST = 'localhost';
-export const API_PROTOCOL = 'http';
-export const API_PORT = 8080;
+export const API_BASE_HOST = 'bt-demo-001.herokuapp.com';
+export const API_PROTOCOL = 'https';
+export const API_PORT = null;
 export const API_BASE_URL = `${API_PROTOCOL}://${API_BASE_HOST}${API_PORT ? ':' + API_PORT : null}${API_PATH}`;
 
 export const BASE_HOST = 'localhost';
@@ -16,5 +16,4 @@ export const PROTOCOL = 'http';
 export const PORT = 4200;
 export const BASE_URL = `${PROTOCOL}://${BASE_HOST}${PORT ? ':' + PORT : null}`;
 
-
-export const BASE_PATH = 'http://localhost:8080/api';
+export const BASE_PATH = new InjectionToken<string>('https://bt-demo-001.herokuapp.com/api');
